ea6ae6a514c7671b11f831b0f25a683d93d34858
Jason Sturges <jason@jasonsturges.com>
2021-02-27 17:23:42 -0600

    Exports

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+export * from "./EventDispatcher";
 export * from "./Test";
+export * from "./TestEvent";
 export * from "./TestResult";
 export * from "./TestResultSet";
 export * from "./TestSuite";
+export * from "./TestSuiteResult";
