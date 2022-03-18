03d9fa40199b7a9a0ed7f643b7eaae6ae16617bf
Jason Sturges <jason@jasonsturges.com>
2022-03-17 22:33:28 -0500

    Exports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { Test } from "./Test";
+export { TestEvent } from "./TestEvent";
+export { TestOptions } from "./TestOptions";
+export { TestResult } from "./TestResult";
+export { TestResultSet } from "./TestResultSet";
+export { TestSuite } from "./TestSuite";
+export { TestSuiteOptions } from "./TestSuiteOptions";
+export { TestSuiteResult } from "./TestSuiteResult";
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-export * from "./EventDispatcher";
-export * from "./Test";
-export * from "./TestEvent";
-export * from "./TestResult";
-export * from "./TestResultSet";
-export * from "./TestSuite";
-export * from "./TestSuiteResult";
